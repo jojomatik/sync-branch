99e7f17bff29d2edd366821edbfa1699c60cb765
jojomatik <dev@jojomatik.de>
2022-08-26 22:32:32 +0200

    ci: update release config
    Update release config to also release a new patch version for updates of production dependencies.

--- release.config.js
+++ release.config.js
@@ -4,12 +4,40 @@ module.exports = {
       "@semantic-release/commit-analyzer",
       {
         preset: "angular",
+        releaseRules: [
+          { type: "build", scope: "deps", release: "patch" },
+        ],
       },
     ],
     [
       "@semantic-release/release-notes-generator",
       {
         preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            {
+              type: "feat",
+              section: "Features",
+              hidden: false,
+            },
+            {
+              type: "fix",
+              section: "Bug Fixes",
+              hidden: false,
+            },
+            {
+              type: "perf",
+              section: "Performance Improvements",
+              hidden: false,
+            },
+            {
+              type: "build",
+              scope: "deps",
+              hidden: false,
+              section: "Dependency updates",
+            },
+          ],
+        },
       },
     ],
     "@semantic-release/github",
